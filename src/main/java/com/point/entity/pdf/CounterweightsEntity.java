c95baa99cc892ba881bbd6885755ef906f9a5ea6
xuyanzhe1111 <38707500+xuyanzhe1111@users.noreply.github.com>
2020-08-25 16:12:31 +0800

    xg

--- src/main/java/com/point/entity/pdf/CounterweightsEntity.java
+++ src/main/java/com/point/entity/pdf/CounterweightsEntity.java
@@ -6,8 +6,11 @@ import lombok.Data;
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.lang.Math.*;
+
 @Data
 public class CounterweightsEntity {
+
     private String no;
     private String ver;
     private String xmmc;
@@ -79,6 +82,8 @@ public class CounterweightsEntity {
     private Double lqg21;
     private Double ltjbpz;
     private Double lbp;
+    private Double dbyj;
+    private Double lbpx;
 
     //    非界面
     private Double dcgssp;
@@ -92,15 +97,110 @@ public class CounterweightsEntity {
     private String qt1;
     private String qt2;
 
+    private String dbType;
+    private String dbModel;
+    private String lzType;
+    private String lzModel;
     private String dcModel;
-    private String dcgssModel;
-    private String btModel;
-    private String ffbbModel;
-    private String trippingModel;
-    private String qsjgModel;
-    private String gruModel;
-    private String yxjgazModel;
     private String dhzjgModel;
+    private String gruModel;
+    private String btModel;
+    private String qsjgModel;
+    private String trippingModel;
+    private String dcgssModel;
+    private String yxjgazModel;
+    private String ffbbModel;
+
+    private Double ma1;
+    private Double ma2;
+    private Double ma3;
+    private Double ma4;
+    private Double ma6;
+    private Double ma20;
+
+    private Double ma5;
+    private Double ma7;
+    private Double ma8;
+    private Double ma9;
+    private Double ma10;
+    private Double ma12;
+    private Double ma13;
+    private Double ma14;
+    private Double ma15;
+    private Double ma16;
+    private Double ma17;
+    private Double ma18;
+    private Double ma19;
+    private Double ma21;
+
+    private Double tsljz;
+    private Double tshljz;
+    private Double mslajz;
+    private Double mshlajz;
+    private Double mqyz;
+    private Double ma11;
+    private Double mkpzyz;
+    private String pzyzy1;
+    private String pzyzm1;
+
+    private Double msla;
+    private Double mshla;
+
+    private Double mq;
+    private Double mkpz;
+    private Double m11s;
+
+    private Double lhl1;
+    private Double lhl2;
+    private Double lhl3;
+    private Double lhl4;
+    private Double lhl5;
+    private Double lhl6;
+    private Double lhl7;
+    private Double lhl8;
+    private Double lhl9;
+    private Double lhl10;
+    private Double lhl11;
+    private Double lhl12;
+    private Double lhl13;
+    private Double lhl14;
+    private Double lhl15;
+    private Double lhl16;
+    private Double lhl17;
+    private Double lhl18;
+    private Double lhl19;
+    private Double lhl20;
+    private Double lhl21;
+    private Double mfhl1;
+    private Double mfhl2;
+    private Double mfhl3;
+    private Double mfhl4;
+    private Double mfhl5;
+    private Double mfhl6;
+    private Double mfhl7;
+    private Double mfhl8;
+    private Double mfhl9;
+    private Double mfhl12;
+    private Double mfhl13;
+    private Double mfhl14;
+    private Double mfhl15;
+    private Double mfhl16;
+    private Double mfhl17;
+    private Double mfhl18;
+    private Double mfhl19;
+    private Double mfhl20;
+    private Double mfhl10;
+    private Double mfhl11;
+    private Double mfhl21;
+    private Double mqhl;
+    private Double mkhl;
+
+    private Double llzgd;
+    private Double llzgdn;
+    private Double llzgdw;
+    private String pzyzy2;
+    private String pzyzm2;
+    private String pzyzm;
 
     public Map<String, String> takeMapForPDF(String type) {
         Map map = new HashMap();
@@ -191,65 +291,117 @@ public class CounterweightsEntity {
         return map;
     }
 
-    public Map<String,String> check(){
-        Map<String,String> map=new HashMap<>();
-        Map<String,String> cp=takeMapForCheckPDF();
-        Map<String,String> p=takeMapForPDF("");
-        map.put("m11s",CommonFunc.convertDoubleToString(p.get("m11s")));
+    public Map<String, String> check() {
+        Map<String, String> map = new HashMap<>();
+        takeMapForCheckPDF();
+        takeMapForPDF("");
+        map.put("m11s", CommonFunc.convertDoubleToString(m11s));
 
-        if( m11==null ) {
+        if (m11 == null) {
             return map;
-        };
-        map.put("m11",CommonFunc.convertDoubleToString(m11));
-        map.put("mqyz",CommonFunc.convertDoubleToString(cp.get("mqyz")));
-        map.put("mkpzyz",CommonFunc.convertDoubleToString(cp.get("mkpzyz")));
-        map.put("pzyzy1",CommonFunc.convertDoubleToString(cp.get("pzyzy1")));
-        map.put("pzyzm1",CommonFunc.convertDoubleToString(cp.get("pzyzm1")));
+        }
+        map.put("m11", CommonFunc.convertDoubleToString(m11));
+        map.put("pzyzy1", CommonFunc.convertDoubleToString(pzyzy1));
+        map.put("pzyzy2", CommonFunc.convertDoubleToString(pzyzy2));
+        map.put("pzyzm", CommonFunc.convertDoubleToString(pzyzm));
         return map;
 
     }
+
     /*
           验证部分
            */
     public Map<String, String> takeMapForCheckPDF() {
         Map<String, Double> map = takeDoubleMapForPdf("");
 
-      if( m11==null ) {
-          return null;
-      };
+        if (m11 == null) {
+            return null;
+        }
 
-        Double ma1 = map.get("ma1");
-        Double ma2 = map.get("ma2");
-        Double ma3 = map.get("ma3");
-        Double ma4 = map.get("ma4");
-        Double ma6 = map.get("ma6");
-        Double ma20 = map.get("ma20");
+        tsljz = swp + 1.5 * r1 + mwr + mffbb;
+        tshljz = 1.25 * hwll + hsw;
+        mslajz = tsljz * lslqg * 10;
+        mshlajz = tshljz * lshlqg * 10;
+        mqyz = mslajz + mshlajz + ma1 + ma2 + ma3 + ma4 + ma6 + ma20;
+        ma11 = m11 * lqg11 * 10;
+        mkpzyz = ma5 + ma7 + ma8 + ma9 + ma10 + ma11 + ma12 + ma13 + ma14 + ma15 + ma16 + ma17 + ma18 + ma19 + ma21;
 
-        Double ma5 = map.get("ma5");
-        Double ma7 = map.get("ma7");
-        Double ma8 = map.get("ma8");
-        Double ma9 = map.get("ma9");
-        Double ma10 = map.get("ma10");
-        Double ma12 = map.get("ma12");
-        Double ma13 = map.get("ma13");
-        Double ma14 = map.get("ma14");
-        Double ma15 = map.get("ma15");
-        Double ma16 = map.get("ma16");
-        Double ma17 = map.get("ma17");
-        Double ma18 = map.get("ma18");
-        Double ma19 = map.get("ma19");
-        Double ma21 = map.get("ma21");
+        pzyzy1 = (mqyz <= mkpzyz) ? "没有" : "有";
+        pzyzm1 = (mqyz <= mkpzyz) ? "符合" : "不符合";
 
-        Double tsljz = swp + 1.5 * r1 + mwr + mffbb;
-        Double tshljz = 1.25 * hwll + hsw;
-        Double mslajz = tsljz * lslqg * 10;
-        Double mshlajz = tshljz * lshlqg * 10;
-        Double mqyz = mslajz + mshlajz + ma1 + ma2 + ma3 + ma4 + ma6 + ma20;
-        Double ma11 = m11 * lqg11 * 10;
-        Double mkpzyz = ma5 + ma7 + ma8 + ma9 + ma10 + ma11 + ma12 + ma13 + ma14 + ma15 + ma16 + ma17 + ma18 + ma19 + ma21;
+        lhl1 = lbx - (ldcgh - lbtzh) + ll / 2;
+        lhl2 = lbx - (ldcgh - ldslzh) + ll / 2;
+        lhl3 = lhl1;
+        if (lzType.indexOf("固定立柱") > -1) lhl4 = lqg4 + lg / 2 + ll / 2;
+        else if (lzType.indexOf("仰臂固定立柱") > -1) lhl4 = lqg4 + lg / 2 / cos(dbyj) + ll / 2;
+        else if (lzType.indexOf("1+1顶升立柱") > -1) lhl4 = lqg4 + lg / 2 + ll / 2;
+        else if (lzType.indexOf("仰柱机构") > -1) lhl4 = (lqg4 + lg / 2) / cos(dbyj) + ll / 2;
 
-        String pzyzy1 = (mqyz <= mkpzyz) ? "没有" : "有";
-        String pzyzm1 = (mqyz <= mkpzyz) ? "符合" : "不符合";
+        if (lzType.indexOf("固定立柱") > -1) lhl5 = lg / 2 - lqg5 + ll / 2;
+        else if (lzType.indexOf("仰臂固定立柱") > -1) lhl5 = (lg / 2 - lqg5) / cos(dbyj) + ll / 2;
+        else if (lzType.indexOf("1+1顶升立柱") > -1) lhl5 = lg / 2 - lqg5 + ll / 2;
+        else if (lzType.indexOf("仰柱机构") > -1) lhl5 = (lg / 2 - lqg5) / cos(dbyj) + ll / 2;
+
+        lhl6 = lg / 2 - lqg6 + ll / 2;
+        lhl7 = lg / 2 - lqg7 + ll / 2;
+        lhl8 = lg / 2 - lqg8 + ll / 2;
+        lhl9 = ll / 2;
+        if (m10 == 0) lhl10 = 0.0;
+        else lhl10 = lqg10 - lg / 2 - ll / 2;
+        lhl11 = lbpx - ll / 2;
+        lhl12 = ll / 2;
+        lhl13 = ll / 2 - lqg13 - lg / 2;
+        if (m14 == 0) lhl14 = 0.0;
+        else lhl14 = ll / 2 - lqg14 - lg / 2;
+        lhl15 = ll / 2;
+        if (trippingModel.equals("无")) lhl16 = 0.0;
+        else lhl16 = ll;
+        if (trippingModel.equals("无")) lhl17 = 0.0;
+        else lhl17 = 0.0;
+        if (trippingModel.equals("无")) lhl18 = 0.0;
+        else lhl18 = ll;
+        if (trippingModel.equals("无")) lhl19 = 0.0;
+        else lhl19 = 0.0;
+
+        if (m20 == 0) lhl20 = 0.0;
+        else lhl20 = lqg20 + lg / 2 + ll / 2;
+        if (m21 == 0) lhl21 = 0.0;
+        else lhl21 = lqg21 - lg / 2 - ll / 2;
+
+        mfhl1 = m1 * lhl1 * 10;
+        mfhl2 = m2 * lhl2 * 10;
+        mfhl3 = m3 * lhl3 * 10;
+        mfhl4 = m4 * lhl4 * 10;
+        mfhl5 = m5 * lhl5 * 10;
+        mfhl6 = m6 * lhl6 * 10;
+        mfhl7 = m7 * lhl7 * 10;
+        mfhl8 = m8 * lhl8 * 10;
+        mfhl9 = m9 * lhl9 * 10;
+        mfhl12 = m12 * lhl12 * 10;
+        mfhl13 = m13 * lhl13 * 10;
+        mfhl14 = m14 * lhl14 * 10;
+        mfhl15 = m15 * lhl15 * 10;
+        mfhl16 = m16 * lhl16 * 10;
+        mfhl17 = m17 * lhl17 * 10;
+        mfhl18 = m18 * lhl18 * 10;
+        mfhl19 = m19 * lhl19 * 10;
+        mfhl20 = m20 * lhl20 * 10;
+        mfhl10 = m10 * lhl10 * 10;
+        mfhl11 = m11 * lhl11 * 10;
+        mfhl21 = m21 * lhl21 * 10;
+        mqhl = mfhl10 + mfhl11 + mfhl21;
+        mkhl = mfhl1 + mfhl2 + mfhl3 + mfhl4 + mfhl5 + mfhl6 + mfhl7 + mfhl8 + mfhl9
+                + mfhl12 + mfhl13 + mfhl14 + mfhl15 + mfhl16 + mfhl17 + mfhl18 + mfhl19
+                + mfhl20;
+
+        if (mqhl <= mkhl) pzyzy2 = "没有";
+        else pzyzy2 = "有";
+        if (mqhl <= mkhl) pzyzm2 = "满足";
+        else pzyzm2 = "不满足";
+        if (pzyzm1.equals("满足") && pzyzm2.equals("满足"))
+            pzyzm = "满足";
+        else
+            pzyzm = "不满足";
 
         Map<String, String> result = new HashMap<>();
         result.put("swp", CommonFunc.convertDoubleToString(swp));
@@ -342,6 +494,54 @@ public class CounterweightsEntity {
         result.put("m11", CommonFunc.convertDoubleToString(m11));
         result.put("lqg11", CommonFunc.convertDoubleToString(lqg11));
 
+        result.put("lhl1", CommonFunc.convertDoubleToString(lhl1));
+        result.put("lhl2", CommonFunc.convertDoubleToString(lhl2));
+        result.put("lhl3", CommonFunc.convertDoubleToString(lhl3));
+        result.put("lhl4", CommonFunc.convertDoubleToString(lhl4));
+        result.put("lhl5", CommonFunc.convertDoubleToString(lhl5));
+        result.put("lhl6", CommonFunc.convertDoubleToString(lhl6));
+        result.put("lhl7", CommonFunc.convertDoubleToString(lhl7));
+        result.put("lhl8", CommonFunc.convertDoubleToString(lhl8));
+        result.put("lhl9", CommonFunc.convertDoubleToString(lhl9));
+        result.put("lhl10", CommonFunc.convertDoubleToString(lhl10));
+        result.put("lhl11", CommonFunc.convertDoubleToString(lhl11));
+        result.put("lhl12", CommonFunc.convertDoubleToString(lhl12));
+        result.put("lhl13", CommonFunc.convertDoubleToString(lhl13));
+        result.put("lhl14", CommonFunc.convertDoubleToString(lhl14));
+        result.put("lhl15", CommonFunc.convertDoubleToString(lhl15));
+        result.put("lhl16", CommonFunc.convertDoubleToString(lhl16));
+        result.put("lhl17", CommonFunc.convertDoubleToString(lhl17));
+        result.put("lhl18", CommonFunc.convertDoubleToString(lhl18));
+        result.put("lhl19", CommonFunc.convertDoubleToString(lhl19));
+        result.put("lhl20", CommonFunc.convertDoubleToString(lhl20));
+        result.put("lhl21", CommonFunc.convertDoubleToString(lhl21));
+        result.put("mfhl1", CommonFunc.convertDoubleToString(mfhl1));
+        result.put("mfhl2", CommonFunc.convertDoubleToString(mfhl2));
+        result.put("mfhl3", CommonFunc.convertDoubleToString(mfhl3));
+        result.put("mfhl4", CommonFunc.convertDoubleToString(mfhl4));
+        result.put("mfhl5", CommonFunc.convertDoubleToString(mfhl5));
+        result.put("mfhl6", CommonFunc.convertDoubleToString(mfhl6));
+        result.put("mfhl7", CommonFunc.convertDoubleToString(mfhl7));
+        result.put("mfhl8", CommonFunc.convertDoubleToString(mfhl8));
+        result.put("mfhl9", CommonFunc.convertDoubleToString(mfhl9));
+        result.put("mfhl12", CommonFunc.convertDoubleToString(mfhl12));
+        result.put("mfhl13", CommonFunc.convertDoubleToString(mfhl13));
+        result.put("mfhl14", CommonFunc.convertDoubleToString(mfhl14));
+        result.put("mfhl15", CommonFunc.convertDoubleToString(mfhl15));
+        result.put("mfhl16", CommonFunc.convertDoubleToString(mfhl16));
+        result.put("mfhl17", CommonFunc.convertDoubleToString(mfhl17));
+        result.put("mfhl18", CommonFunc.convertDoubleToString(mfhl18));
+        result.put("mfhl19", CommonFunc.convertDoubleToString(mfhl19));
+        result.put("mfhl20", CommonFunc.convertDoubleToString(mfhl20));
+        result.put("mfhl10", CommonFunc.convertDoubleToString(mfhl10));
+        result.put("mfhl11", CommonFunc.convertDoubleToString(mfhl11));
+        result.put("mfhl21", CommonFunc.convertDoubleToString(mfhl21));
+        result.put("mqhl", CommonFunc.convertDoubleToString(mqhl));
+        result.put("mkhl", CommonFunc.convertDoubleToString(mkhl));
+        result.put("pzyzy2", CommonFunc.convertDoubleToString(pzyzy2));
+        result.put("pzyzm2", CommonFunc.convertDoubleToString(pzyzm2));
+        result.put("pzyzm", CommonFunc.convertDoubleToString(pzyzm));
+
         return result;
     }
 
@@ -354,33 +554,32 @@ public class CounterweightsEntity {
          /*
         倾覆力矩和抗倾覆力矩
          */
-        Double msla = tsl * lslqg * 10;
-        Double mshla = tshl * lshlqg * 10;
-        Double ma1 = m1 * lqg1 * 10;
-        Double ma2 = m2 * lqg2 * 10;
-        Double ma3 = m3 * lqg3 * 10;
-        Double ma4 = m4 * lqg4 * 10;
-        Double ma5 = m5 * lqg5 * 10;
-        Double ma6 = m6 * lqg6 * 10;
-        Double ma7 = m7 * lqg7 * 10;
-        Double ma8 = m8 * lqg8 * 10;
-        Double ma9 = m9 * lqg9 * 10;
-        Double ma10 = m10 * lqg10 * 10;
-//        Double ma11= m11*lqg11*10;
-        Double ma12 = m12 * lqg12 * 10;
-        Double ma13 = m13 * lqg13 * 10;
-        Double ma14 = m14 * lqg14 * 10;
-        Double ma15 = m15 * lqg15 * 10;
-        Double ma16 = m16 * lqg16 * 10;
-        Double ma17 = m17 * lqg17 * 10;
-        Double ma18 = m18 * lqg18 * 10;
-        Double ma19 = m19 * lqg19 * 10;
-        Double ma20 = m20 * lqg20 * 10;
-        Double ma21 = m21 * lqg21 * 10;
+        msla = tsl * lslqg * 10;
+        mshla = tshl * lshlqg * 10;
+        ma1 = m1 * lqg1 * 10;
+        ma2 = m2 * lqg2 * 10;
+        ma3 = m3 * lqg3 * 10;
+        ma4 = m4 * lqg4 * 10;
+        ma5 = m5 * lqg5 * 10;
+        ma6 = m6 * lqg6 * 10;
+        ma7 = m7 * lqg7 * 10;
+        ma8 = m8 * lqg8 * 10;
+        ma9 = m9 * lqg9 * 10;
+        ma10 = m10 * lqg10 * 10;
+        ma12 = m12 * lqg12 * 10;
+        ma13 = m13 * lqg13 * 10;
+        ma14 = m14 * lqg14 * 10;
+        ma15 = m15 * lqg15 * 10;
+        ma16 = m16 * lqg16 * 10;
+        ma17 = m17 * lqg17 * 10;
+        ma18 = m18 * lqg18 * 10;
+        ma19 = m19 * lqg19 * 10;
+        ma20 = m20 * lqg20 * 10;
+        ma21 = m21 * lqg21 * 10;
 
-        Double mq = msla + mshla + ma1 + ma2 + ma3 + ma4 + ma6 + ma20;
-        Double mkpz = ma5 + ma7 + ma8 + ma9 + ma10 + ma12 + ma13 + ma14 + ma15 + ma16 + ma17 + ma18 + ma19 + ma21;
-        Double m11s = (npz * mq - mkpz) / (lqg11 * 10);
+        mq = msla + mshla + ma1 + ma2 + ma3 + ma4 + ma6 + ma20;
+        mkpz = ma5 + ma7 + ma8 + ma9 + ma10 + ma12 + ma13 + ma14 + ma15 + ma16 + ma17 + ma18 + ma19 + ma21;
+        m11s = (npz * mq - mkpz) / (lqg11 * 10);
 
         map.put("msla", msla);
         map.put("mshla", mshla);
