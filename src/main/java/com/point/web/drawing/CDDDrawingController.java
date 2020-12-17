1ac7e1875cabef5e4df414a9715c4513affb5565
xuyanzhe1111 <38707500+xuyanzhe1111@users.noreply.github.com>
2020-12-17 22:57:37 +0800

    修改

--- src/main/java/com/point/web/drawing/CDDDrawingController.java
+++ src/main/java/com/point/web/drawing/CDDDrawingController.java
@@ -2,6 +2,7 @@ package com.point.web.drawing;
 
 import com.itextpdf.text.DocumentException;
 import com.point.common.CacheData;
+import com.point.common.CommonFunc;
 import com.point.common.ImageCacheData;
 import com.point.common.URLCacheData;
 import com.point.entity.InputURLCache;
@@ -11,15 +12,14 @@ import com.point.newPDF.controller.DrawingTableController;
 import com.point.web.newController.Tool.ToolForPDFController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.lang.reflect.Field;
+import java.util.*;
 
 @Controller
 public class CDDDrawingController {
@@ -64,23 +64,36 @@ public class CDDDrawingController {
     public Object janyan(@ModelAttribute CDDDrawingEntity entity) {
         return entity.takeMapForPDF("");
     }
-
+    private Map entity2Map(Object entity){
+        Class clazz = entity.getClass();
+        Field[] fields = clazz.getDeclaredFields();
+        Map<String, String> map = new HashMap<>();
+        Iterator<Field> it = CollectionUtils.arrayToList(fields).iterator();
+        try {
+            while (it.hasNext()) {
+                Field f = it.next();
+                f.setAccessible(true);
+                String v = "";
+                if(f.getType().getName().toString().equals("java.lang.Double"))
+                    v= CommonFunc.convertDoubleToString(Double.parseDouble(f.get(entity).toString()));
+                else
+                    v=f.get(entity).toString();
+                String name = f.getName();
+                map.put(name, v);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return map;
+    }
     @PostMapping("CDDDrawing/getPDF")
     public Object getPDF(HttpServletRequest request,
                          @ModelAttribute CDDDrawingEntity entity,
                          @RequestParam(required = false) String pdfType,
                          @RequestParam(required = false) Boolean check,
                          @RequestParam(required = false) String sjht) throws IOException, DocumentException {
-
-        Map<String, File> imageMap = new HashMap<>();
-        try {
-            imageMap = tool.getImageMap(sjht, request);
-        } catch (Exception e) {
-        }
-        urlCacheData.saveCacheValue(sjht, imageMap);
         cacheData.saveCacheValue(sjht, entity);
         List<byte[]> list = new ArrayList<>();
-
         String name = "";
         if ( entity.getCddxhModel().equals("CDD240(2A)") ) {
             name = "CDD240（2A）";
@@ -89,12 +102,8 @@ public class CDDDrawingController {
         } else if ( entity.getCddxhModel().equals("CDD240(4A)") ) {
             name = "CDD240（4A）";
         }
-        list.add(pdf.fromPDFTempletToPdfWithValue_New(entity.takeMapForPDF(pdfType), imageMap, "new/" + name));
-        if (null != check && check) {
-            list.add(pdf.fromPDFTempletToPdfWithValue_New(entity.takeMapForPDF(pdfType), imageMap, "new/CDD240（2A）"));
-            list.add(pdf.fromPDFTempletToPdfWithValue_New(entity.takeMapForPDF(pdfType), imageMap, "new/CDD240（3A）"));
-            list.add(pdf.fromPDFTempletToPdfWithValue_New(entity.takeMapForPDF(pdfType), imageMap, "new/CDD240（4A）"));
-        }
+        Map map= entity2Map(entity);
+        list.add(pdf.fromPDFTempletToPdfWithValue_Drawing(map, null, "/CDD/" + name));
         return tool.getResponseEntity(name, list.get(0));
     }
 }
